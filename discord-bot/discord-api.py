73388068e7bc93ed59d9205c055e0d7ded1208a4
Ayman <ayman.patel97@gmail.com>
2021-01-24 21:10:43 +0530

    Update discord-api.py

--- discord-bot/discord-api.py
+++ discord-bot/discord-api.py
@@ -4,8 +4,7 @@ import discord
 
 from dotenv import load_dotenv
 load_dotenv()
-DISCORD_TOKEN = os.getenv('DISCORD_TOKEN') # TODO: Add env variable
-# DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
+DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
 
 print(DISCORD_TOKEN)
 
